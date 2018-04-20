523aa4254b996a0a05939e6cee16950f1a327a6e
luccasr73 <luccasr83@gmail.com>
2018-04-20 16:56:06 -0300

    Initial commit

---
+++ ionic-escpos-bluetooth-printer-sample/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { BtPrinterProvider } from '../providers/bt-printer/bt-printer';
+import { BluetoothSerial } from '@ionic-native/bluetooth-serial';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    BluetoothSerial,
+    BtPrinterProvider
+  ]
+})
+export class AppModule {}
