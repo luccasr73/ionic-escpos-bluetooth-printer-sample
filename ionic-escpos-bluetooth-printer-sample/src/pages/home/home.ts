fcfbf9867941bbc720c800d060c1d869baead6be
luccasr73 <luccasr83@gmail.com>
2018-07-20 23:08:19 -0300

    Improved code flow

--- ionic-escpos-bluetooth-printer-sample/src/pages/home/home.ts
+++ ionic-escpos-bluetooth-printer-sample/src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController, AlertController} from 'ionic-angular';
-import { BtPrinterProvider } from '../../providers/bt-printer/bt-printer';
+import { AlertController, LoadingController, NavController, ToastController } from 'ionic-angular';
+import { PrinterProvider } from './../../providers/printer/printer';
+import { commands } from './../../providers/printer/printer-commands';
 
 @Component({
   selector: 'page-home',
@@ -9,196 +10,188 @@ import { BtPrinterProvider } from '../../providers/bt-printer/bt-printer';
 export class HomePage {
   arrDevices: any = [];
   commands: any;
-  constructor(public navCtrl: NavController, private btPrinterProvider: BtPrinterProvider, private alertCtrl: AlertController) {
-    //commands based on https://github.com/humbertopiaia/escpos-commands-js/blob/master/src/commands.js
-    this.commands = {
-      LF: '\x0a',
-      ESC: '\x1b',
-      FS: '\x1c',
-      GS: '\x1d',
-      US: '\x1f',
-      FF: '\x0c',
-      DLE: '\x10',
-      DC1: '\x11',
-      DC4: '\x14',
-      EOT: '\x04',
-      NUL: '\x00',
-      EOL: '\n',
-      FEED_CONTROL_SEQUENCES: {
-        CTL_LF: '\x0a', // Print and line feed
-        CTL_FF: '\x0c', // Form feed
-        CTL_CR: '\x0d', // Carriage return
-        CTL_HT: '\x09', // Horizontal tab
-        CTL_VT: '\x0b', // Vertical tab
-      },
-      LINE_SPACING: {
-        LS_DEFAULT: '\x1b\x32',
-        LS_SET: '\x1b\x33'
-      },
-      HARDWARE: {
-        HW_INIT: '\x1b\x40', // Clear data in buffer and reset modes
-        HW_SELECT: '\x1b\x3d\x01', // Printer select
-        HW_RESET: '\x1b\x3f\x0a\x00', // Reset printer hardware
-      },
-      CASH_DRAWER: {
-        CD_KICK_2: '\x1b\x70\x00', // Sends a pulse to pin 2 []
-        CD_KICK_5: '\x1b\x70\x01', // Sends a pulse to pin 5 []
-      },
-      MARGINS: {
-        BOTTOM: '\x1b\x4f', // Fix bottom size
-        LEFT: '\x1b\x6c', // Fix left size
-        RIGHT: '\x1b\x51', // Fix right size
-      },
-      PAPER: {
-        PAPER_FULL_CUT: '\x1d\x56\x00', // Full cut paper
-        PAPER_PART_CUT: '\x1d\x56\x01', // Partial cut paper
-        PAPER_CUT_A: '\x1d\x56\x41', // Partial cut paper
-        PAPER_CUT_B: '\x1d\x56\x42', // Partial cut paper
-      },
-      TEXT_FORMAT: {
-        TXT_NORMAL: '\x1b\x21\x00', // Normal text
-        TXT_2HEIGHT: '\x1b\x21\x10', // Double height text
-        TXT_2WIDTH: '\x1b\x21\x20', // Double width text
-        TXT_4SQUARE: '\x1b\x21\x30', // Double width & height text
+  constructor(public navCtrl: NavController, private print: PrinterProvider, private alertCtrl: AlertController, private loadCtrl: LoadingController, private toastCtrl: ToastController) {}
 
-        TXT_CUSTOM_SIZE: function (width, height) { // other sizes
-          var widthDec = (width - 1) * 16;
-          var heightDec = height - 1;
-          var sizeDec = widthDec + heightDec;
-          return '\x1d\x21' + String.fromCharCode(sizeDec);
-        },
-
-        TXT_HEIGHT: {
-          1: '\x00',
-          2: '\x01',
-          3: '\x02',
-          4: '\x03',
-          5: '\x04',
-          6: '\x05',
-          7: '\x06',
-          8: '\x07'
-        },
-        TXT_WIDTH: {
-          1: '\x00',
-          2: '\x10',
-          3: '\x20',
-          4: '\x30',
-          5: '\x40',
-          6: '\x50',
-          7: '\x60',
-          8: '\x70'
-        },
-
-        TXT_UNDERL_OFF: '\x1b\x2d\x00', // Underline font OFF
-        TXT_UNDERL_ON: '\x1b\x2d\x01', // Underline font 1-dot ON
-        TXT_UNDERL2_ON: '\x1b\x2d\x02', // Underline font 2-dot ON
-        TXT_BOLD_OFF: '\x1b\x45\x00', // Bold font OFF
-        TXT_BOLD_ON: '\x1b\x45\x01', // Bold font ON
-        TXT_ITALIC_OFF: '\x1b\x35', // Italic font ON
-        TXT_ITALIC_ON: '\x1b\x34', // Italic font ON
-
-        TXT_FONT_A: '\x1b\x4d\x00', // Font type A
-        TXT_FONT_B: '\x1b\x4d\x01', // Font type B
-        TXT_FONT_C: '\x1b\x4d\x02', // Font type C
-
-        TXT_ALIGN_LT: '\x1b\x61\x00', // Left justification
-        TXT_ALIGN_CT: '\x1b\x61\x01', // Centering
-        TXT_ALIGN_RT: '\x1b\x61\x02', // Right justification
-      }
-    };
+  showToast(data) {
+    let toast = this.toastCtrl.create({
+      duration: 3000,
+      message: data,
+      position: 'bottom'
+    });
+    toast.present();
   }
-  print() {
-    this.btPrinterProvider.enableBt()
-      .then(() => this.btPrinterProvider.searchBt()
-        .then((devicesList) => {
-          // Open printer selection
-          let alert = this.alertCtrl.create({
-            title: 'Select your bluetooth printer',
-            buttons: [{
-                text: 'CANCEL',
-                role: 'cancel'
-              },
-              {
-                text: 'SELECT',
-                handler: (data) => {
-                  this.arrDevices = data;
-                  this.btPrinterProvider.connectBt(this.arrDevices).subscribe((connectStatus) => {
-                    console.log("Connected", connectStatus);
-                    // based on http://ciaranj.blogspot.fr/2007/11/utf8-characters-encoding-in-javascript.html
-                    //this allows the use of accents and special characters
-                    function stringToBytes(string) {
 
-                      var bytes = [];
-                      for (var n = 0; n < string.length; n++) {
-                        var c = string.charCodeAt(n);
-                        if (c < 128) {
+  noSpecialChars(string) {
+    var translate = {
+        "à": "a",
+        "á": "a",
+        "â": "a",
+        "ã": "a",
+        "ä": "a",
+        "å": "a",
+        "æ": "a",
+        "ç": "c",
+        "è": "e",
+        "é": "e",
+        "ê": "e",
+        "ë": "e",
+        "ì": "i",
+        "í": "i",
+        "î": "i",
+        "ï": "i",
+        "ð": "d",
+        "ñ": "n",
+        "ò": "o",
+        "ó": "o",
+        "ô": "o",
+        "õ": "o",
+        "ö": "o",
+        "ø": "o",
+        "ù": "u",
+        "ú": "u",
+        "û": "u",
+        "ü": "u",
+        "ý": "y",
+        "þ": "b",
+        "ÿ": "y",
+        "ŕ": "r",
+        "À": "A",
+        "Á": "A",
+        "Â": "A",
+        "Ã": "A",
+        "Ä": "A",
+        "Å": "A",
+        "Æ": "A",
+        "Ç": "C",
+        "È": "E",
+        "É": "E",
+        "Ê": "E",
+        "Ë": "E",
+        "Ì": "I",
+        "Í": "I",
+        "Î": "I",
+        "Ï": "I",
+        "Ð": "D",
+        "Ñ": "N",
+        "Ò": "O",
+        "Ó": "O",
+        "Ô": "O",
+        "Õ": "O",
+        "Ö": "O",
+        "Ø": "O",
+        "Ù": "U",
+        "Ú": "U",
+        "Û": "U",
+        "Ü": "U",
+        "Ý": "Y",
+        "Þ": "B",
+        "Ÿ": "Y",
+        "Ŕ": "R"
+      },
+      translate_re = /[àáâãäåæçèéêëìíîïðñòóôõöøùúûüýþßàáâãäåæçèéêëìíîïðñòóôõöøùúûýýþÿŕŕÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÝÝÞŸŔŔ]/gim;
+    return (string.replace(translate_re, function (match) {
+      return translate[match];
+    }));
+  }
 
-                          bytes[bytes.length] = c;
-
-                        } else if ((c > 127) && (c < 2048)) {
-
-                          bytes[bytes.length] = (c >> 6) | 192;
-                          bytes[bytes.length] = (c & 63) | 128;
-
-                        } else {
-
-                          bytes[bytes.length] = (c >> 12) | 224;
-                          bytes[bytes.length] = ((c >> 6) & 63) | 128;
-                          bytes[bytes.length] = (c & 63) | 128;
-                        }
-                      }
-                      return new Uint8Array(bytes);
-                    }
-                    //receipt +='';
-
-                    var receipt = '';
-                    receipt += this.commands.HARDWARE.HW_INIT;
-                    receipt += this.commands.TEXT_FORMAT.TXT_ALIGN_CT;
-                    receipt += this.commands.TEXT_FORMAT.TXT_4SQUARE;
-                    receipt += 'Printer test';
-                    receipt += this.commands.EOL;
-                    receipt += this.commands.TEXT_FORMAT.TXT_NORMAL;
-                    receipt += 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tellus sapien, aliquam id mattis et, pretium eu libero. In dictum mauris vel lorem porttitor, et tempor neque semper. Aliquam erat volutpat. Aliquam vel malesuada urna, a pulvinar augue. Nunc ac fermentum massa. Proin efficitur purus fermentum tellus fringilla, fringilla aliquam nunc dignissim. Duis et luctus tellus, sed ullamcorper lectus.'
-                    receipt += this.commands.PAPER.PAPER_FULL_CUT;
-
-
-                    this.btPrinterProvider.printBt(stringToBytes(receipt)).then((printStatus) => {
-                      console.log("Successful printing", printStatus);
-                      let alertPrinterSuccess = this.alertCtrl.create({
-                        title: "Successful printing",
-                        buttons: ['CLOSE']
-                      });
-                      alertPrinterSuccess.present();
-                      this.btPrinterProvider.disconectBt();
-                    }, (error) => {
-                      let alertPrinterError = this.alertCtrl.create({
-                        title: "Printing not performed, error: " + error,
-                        buttons: ['CLOSE']
-                      });
-                      alertPrinterError.present();
-                    });
-
-                  }, (error) => {
-                    let alertPrinterError = this.alertCtrl.create({
-                      title: "Please select a printer",
-                      buttons: ['CLOSE']
-                    });
-                    alertPrinterError.present();
-                  });
-                }
-              }
-            ]
-          });
-
-          devicesList.forEach((devicesList) => {
-            alert.addInput({
-              name: 'printer',
-              value: devicesList.address,
-              label: devicesList.name,
-              type: 'radio'
+  printData(device, data) {
+    console.log('Device mac: ', device);
+    console.log('Data: ', data);
+    let load = this.loadCtrl.create({
+      content: 'Printing...'
+    });
+    load.present();
+    this.print.connectBluetooth(device).subscribe(status => {
+        console.log(status);
+        this.print.printBluetooth(this.noSpecialChars(data))
+          .then(printStatus => {
+            console.log(printStatus);
+            let alert = this.alertCtrl.create({
+              title: 'Successful print!',
+              buttons: ['Ok']
             });
+            load.dismiss();
+            alert.present();
+            this.print.disconnectBluetooth();
+          })
+          .catch(error => {
+            console.log(error);
+            let alert = this.alertCtrl.create({
+              title: 'There was an error printing, please try again!',
+              buttons: ['Ok']
+            });
+            load.dismiss();
+            alert.present();
+            this.print.disconnectBluetooth();
           });
-          alert.present();
-        }).catch((error) => console.log(error)));
+      },
+      error => {
+        console.log(error);
+        let alert = this.alertCtrl.create({
+          title: 'There was an error connecting to the printer, please try again!',
+          buttons: ['Ok']
+        });
+        load.dismiss();
+        alert.present();
+      });
+  }
+
+  prepareToPrint() {
+    let receipt = '';
+    receipt += commands.HARDWARE.HW_INIT;
+    receipt += commands.TEXT_FORMAT.TXT_4SQUARE;
+    receipt += commands.TEXT_FORMAT.TXT_ALIGN_CT;
+    receipt += 'TITLE';
+    receipt += commands.EOL;
+    receipt += commands.TEXT_FORMAT.TXT_NORMAL;
+    receipt += commands.HORIZONTAL_LINE.HR_48MM;
+    receipt += commands.EOL;
+    receipt += commands.TEXT_FORMAT.TXT_ALIGN_LT;
+    receipt += 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tellus sapien, aliquam id mattis et, pretium eu libero. In dictum mauris vel lorem porttitor, et tempor neque semper. Aliquam erat volutpat. Aliquam vel malesuada urna, a pulvinar augue. Nunc ac fermentum massa. Proin efficitur purus fermentum tellus fringilla, fringilla aliquam nunc dignissim. Duis et luctus tellus, sed ullamcorper lectus.';
+    //secure space on footer
+    receipt += commands.EOL;
+    receipt += commands.EOL;
+    receipt += commands.EOL;
+
+    let alert = this.alertCtrl.create({
+      title: 'Select your printer',
+      buttons: [{
+          text: 'Cancel',
+          role: 'cancel'
+        },
+        {
+          text: 'Select printer',
+          handler: (device) => {
+            if(!device){
+              this.showToast('Select a printer!');
+              return ;
+            }
+            console.log(device);
+            this.printData(device, receipt);
+          }
+        }
+      ]
+    });
+
+    this.print.enableBluetooth().then(() => {
+      this.print.searchBluetooth().then(devices => {
+        devices.forEach((device) => {
+          console.log('Devices: ', JSON.stringify(device));
+          alert.addInput({
+            name: 'printer',
+            value: device.address,
+            label: device.name,
+            type: 'radio'
+          });
+        });
+        alert.present();
+      }).catch((error) => {
+        console.log(error);
+        this.showToast('There was an error connecting the printer, please try again!');
+      });
+    }).catch((error) => {
+      console.log(error);
+      this.showToast('Error activating bluetooth, please try again!');
+    });
   }
 }
