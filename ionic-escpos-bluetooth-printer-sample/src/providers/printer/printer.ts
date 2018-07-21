b98977d8caf9a30a29da26483c45d5b653d6dd7b
luccasr73 <luccasr83@gmail.com>
2018-07-20 23:06:44 -0300

    Renamed to printer

---
+++ ionic-escpos-bluetooth-printer-sample/src/providers/printer/printer.ts
@@ -0,0 +1,29 @@
+import { BluetoothSerial } from '@ionic-native/bluetooth-serial';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class PrinterProvider {
+
+  constructor(private bluetoothSerial: BluetoothSerial) {}
+
+  enableBluetooth(){
+    return this.bluetoothSerial.enable();
+  }
+
+  searchBluetooth(){
+    return this.bluetoothSerial.list();
+  }
+
+  connectBluetooth(address){
+    return this.bluetoothSerial.connect(address);
+  }
+
+  printBluetooth(data){
+    return this.bluetoothSerial.write(data);
+  }
+
+  disconnectBluetooth(){
+    return this.bluetoothSerial.disconnect();
+  }
+
+}
