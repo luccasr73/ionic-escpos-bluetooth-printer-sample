b40d6f48f3b1f75e7cddc3b0c731083ea9a557cf
luccasr73 <luccasrobert@hotmail.com>
2019-11-16 22:37:58 -0300

    fix #6

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import {
+  Component
+} from '@angular/core';
 import {
   AlertController,
   LoadingController,
   NavController,
   ToastController,
 } from 'ionic-angular';
-import { PrinterProvider } from './../../providers/printer/printer';
-import { commands } from './../../providers/printer/printer-commands';
-
+import {
+  PrinterProvider
+} from './../../providers/printer/printer';
+import {
+  commands
+} from './../../providers/printer/printer-commands';
+import EscPosEncoder from 'esc-pos-encoder-ionic';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html',
@@ -31,83 +37,83 @@ export class HomePage {
     });
     toast.present();
   }
-
-  noSpecialChars(string) {
-    var translate = {
-        à: 'a',
-        á: 'a',
-        â: 'a',
-        ã: 'a',
-        ä: 'a',
-        å: 'a',
-        æ: 'a',
-        ç: 'c',
-        è: 'e',
-        é: 'e',
-        ê: 'e',
-        ë: 'e',
-        ì: 'i',
-        í: 'i',
-        î: 'i',
-        ï: 'i',
-        ð: 'd',
-        ñ: 'n',
-        ò: 'o',
-        ó: 'o',
-        ô: 'o',
-        õ: 'o',
-        ö: 'o',
-        ø: 'o',
-        ù: 'u',
-        ú: 'u',
-        û: 'u',
-        ü: 'u',
-        ý: 'y',
-        þ: 'b',
-        ÿ: 'y',
-        ŕ: 'r',
-        À: 'A',
-        Á: 'A',
-        Â: 'A',
-        Ã: 'A',
-        Ä: 'A',
-        Å: 'A',
-        Æ: 'A',
-        Ç: 'C',
-        È: 'E',
-        É: 'E',
-        Ê: 'E',
-        Ë: 'E',
-        Ì: 'I',
-        Í: 'I',
-        Î: 'I',
-        Ï: 'I',
-        Ð: 'D',
-        Ñ: 'N',
-        Ò: 'O',
-        Ó: 'O',
-        Ô: 'O',
-        Õ: 'O',
-        Ö: 'O',
-        Ø: 'O',
-        Ù: 'U',
-        Ú: 'U',
-        Û: 'U',
-        Ü: 'U',
-        Ý: 'Y',
-        Þ: 'B',
-        Ÿ: 'Y',
-        Ŕ: 'R',
-      },
-      translate_re = /[àáâãäåæçèéêëìíîïðñòóôõöøùúûüýþßàáâãäåæçèéêëìíîïðñòóôõöøùúûýýþÿŕŕÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÝÝÞŸŔŔ]/gim;
-    return string.replace(translate_re, function(match) {
-      return translate[match];
-    });
-  }
-
+  /*
+    noSpecialChars(string) {
+      var translate = {
+          à: 'a',
+          á: 'a',
+          â: 'a',
+          ã: 'a',
+          ä: 'a',
+          å: 'a',
+          æ: 'a',
+          ç: 'c',
+          è: 'e',
+          é: 'e',
+          ê: 'e',
+          ë: 'e',
+          ì: 'i',
+          í: 'i',
+          î: 'i',
+          ï: 'i',
+          ð: 'd',
+          ñ: 'n',
+          ò: 'o',
+          ó: 'o',
+          ô: 'o',
+          õ: 'o',
+          ö: 'o',
+          ø: 'o',
+          ù: 'u',
+          ú: 'u',
+          û: 'u',
+          ü: 'u',
+          ý: 'y',
+          þ: 'b',
+          ÿ: 'y',
+          ŕ: 'r',
+          À: 'A',
+          Á: 'A',
+          Â: 'A',
+          Ã: 'A',
+          Ä: 'A',
+          Å: 'A',
+          Æ: 'A',
+          Ç: 'C',
+          È: 'E',
+          É: 'E',
+          Ê: 'E',
+          Ë: 'E',
+          Ì: 'I',
+          Í: 'I',
+          Î: 'I',
+          Ï: 'I',
+          Ð: 'D',
+          Ñ: 'N',
+          Ò: 'O',
+          Ó: 'O',
+          Ô: 'O',
+          Õ: 'O',
+          Ö: 'O',
+          Ø: 'O',
+          Ù: 'U',
+          Ú: 'U',
+          Û: 'U',
+          Ü: 'U',
+          Ý: 'Y',
+          Þ: 'B',
+          Ÿ: 'Y',
+          Ŕ: 'R',
+        },
+        translate_re = /[àáâãäåæçèéêëìíîïðñòóôõöøùúûüýþßàáâãäåæçèéêëìíîïðñòóôõöøùúûýýþÿŕŕÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÝÝÞŸŔŔ]/gim;
+      return string.replace(translate_re, function(match) {
+        return translate[match];
+      });
+    }
+  */
   print(device, data) {
     console.log('Device mac: ', device);
-    console.log('Data: ', data);
+    console.log('Data: ', JSON.stringify(data));
     let load = this.loadCtrl.create({
       content: 'Printing...',
     });
@@ -116,20 +122,18 @@ export class HomePage {
       (status) => {
         console.log(status);
         this.printer
-          .printData(this.noSpecialChars(data))
+          .printData(data)
           .then((printStatus) => {
             console.log(printStatus);
             let alert = this.alertCtrl.create({
               title: 'Successful print!',
-              buttons: [
-                {
-                  text: 'Ok',
-                  handler: () => {
-                    load.dismiss();
-                    this.printer.disconnectBluetooth();
-                  },
+              buttons: [{
+                text: 'Ok',
+                handler: () => {
+                  load.dismiss();
+                  this.printer.disconnectBluetooth();
                 },
-              ],
+              }, ],
             });
             alert.present();
           })
@@ -137,15 +141,13 @@ export class HomePage {
             console.log(error);
             let alert = this.alertCtrl.create({
               title: 'There was an error printing, please try again!',
-              buttons: [
-                {
-                  text: 'Ok',
-                  handler: () => {
-                    load.dismiss();
-                    //this.printer.disconnectBluetooth();
-                  },
+              buttons: [{
+                text: 'Ok',
+                handler: () => {
+                  load.dismiss();
+                  //this.printer.disconnectBluetooth();
                 },
-              ],
+              }, ],
             });
             alert.present();
           });
@@ -153,17 +155,14 @@ export class HomePage {
       (error) => {
         console.log(error);
         let alert = this.alertCtrl.create({
-          title:
-            'There was an error connecting to the printer, please try again!',
-          buttons: [
-            {
-              text: 'Ok',
-              handler: () => {
-                load.dismiss();
-                //this.printer.disconnectBluetooth();
-              },
+          title: 'There was an error connecting to the printer, please try again!',
+          buttons: [{
+            text: 'Ok',
+            handler: () => {
+              load.dismiss();
+              //this.printer.disconnectBluetooth();
             },
-          ],
+          }, ],
         });
         alert.present();
       },
@@ -179,34 +178,53 @@ export class HomePage {
       data.text =
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tellus sapien, aliquam id mattis et, pretium eu libero. In dictum mauris vel lorem porttitor, et tempor neque semper. Aliquam erat volutpat. Aliquam vel malesuada urna, a pulvinar augue. Nunc ac fermentum massa. Proin efficitur purus fermentum tellus fringilla, fringilla aliquam nunc dignissim. Duis et luctus tellus, sed ullamcorper lectus.';
     }
-
-    let receipt = '';
-    receipt += commands.HARDWARE.HW_INIT;
-    receipt += commands.TEXT_FORMAT.TXT_4SQUARE;
-    receipt += commands.TEXT_FORMAT.TXT_ALIGN_CT;
-    receipt += data.title.toUpperCase();
-    receipt += commands.EOL;
-    receipt += commands.TEXT_FORMAT.TXT_NORMAL;
-    receipt += commands.HORIZONTAL_LINE.HR_58MM;
-    receipt += commands.EOL;
-    receipt += commands.HORIZONTAL_LINE.HR2_58MM;
-    receipt += commands.EOL;
-    receipt += commands.TEXT_FORMAT.TXT_ALIGN_LT;
-    receipt += data.text;
-    //secure space on footer
-    receipt += commands.EOL;
-    receipt += commands.EOL;
-    receipt += commands.EOL;
+    /*
+        let receipt = '';
+        receipt += commands.HARDWARE.HW_INIT;
+        receipt += commands.TEXT_FORMAT.TXT_4SQUARE;
+        receipt += commands.TEXT_FORMAT.TXT_ALIGN_CT;
+        receipt += data.title.toUpperCase();
+        receipt += commands.EOL;
+        receipt += commands.TEXT_FORMAT.TXT_NORMAL;
+        receipt += commands.HORIZONTAL_LINE.HR_58MM;
+        receipt += commands.EOL;
+        receipt += commands.HORIZONTAL_LINE.HR2_58MM;
+        receipt += commands.EOL;
+        receipt += commands.TEXT_FORMAT.TXT_ALIGN_LT;
+        receipt += data.text;
+        //secure space on footer
+        receipt += commands.EOL;
+        receipt += commands.EOL;
+        receipt += commands.EOL;*/
     //this.receipt = receipt;
-    this.mountAlertBt(receipt);
+    const encoder = new EscPosEncoder();
+    const result = encoder.initialize();
+
+    result
+      .codepage('cp936')
+      .align('center')
+      .raw(commands.TEXT_FORMAT.TXT_4SQUARE)
+      .line(data.title)
+      .raw(commands.TEXT_FORMAT.TXT_NORMAL)
+      .text(commands.HORIZONTAL_LINE.HR_58MM)
+      .text(commands.HORIZONTAL_LINE.HR2_58MM)
+      .text(data.text)
+      .newline()
+      .raw(commands.TEXT_FORMAT.TXT_4SQUARE)
+      .text('简体中文')
+      .newline()
+      .newline()
+      .newline()
+
+
+    this.mountAlertBt(result.encode());
   }
 
   mountAlertBt(data) {
     this.receipt = data;
     let alert = this.alertCtrl.create({
       title: 'Select your printer',
-      buttons: [
-        {
+      buttons: [{
           text: 'Cancel',
           role: 'cancel',
         },
--- src/providers/printer/printer-commands.ts
+++ src/providers/printer/printer-commands.ts
@@ -1,98 +1,98 @@
 //commands based on https://github.com/humbertopiaia/escpos-commands-js/blob/master/src/commands.js
 //all the commands below may vary by printer, check the manual
 export const commands = {
-  LF: '\x0a',
-  ESC: '\x1b',
-  FS: '\x1c',
-  GS: '\x1d',
-  US: '\x1f',
-  FF: '\x0c',
-  DLE: '\x10',
-  DC1: '\x11',
-  DC4: '\x14',
-  EOT: '\x04',
-  NUL: '\x00',
-  EOL: '\n',
+  LF: [0x0a],
+  ESC: [0x1b],
+  FS: [0x1c],
+  GS: [0x1d],
+  US: [0x1f],
+  FF: [0x0c],
+  DLE: [0x10],
+  DC1: [0x11],
+  DC4: [0x14],
+  EOT: [0x04],
+  NUL: [0x00],
+  //EOL: [\n],
   HORIZONTAL_LINE: {
     HR_58MM: '================================',
     HR2_58MM: '********************************'
   },
   FEED_CONTROL_SEQUENCES: {
-    CTL_LF: '\x0a', // Print and line feed
-    CTL_FF: '\x0c', // Form feed
-    CTL_CR: '\x0d', // Carriage return
-    CTL_HT: '\x09', // Horizontal tab
-    CTL_VT: '\x0b', // Vertical tab
+    CTL_LF: [0x0a], // Print and line feed
+    CTL_FF: [0x0c], // Form feed
+    CTL_CR: [0x0d], // Carriage return
+    CTL_HT: [0x09], // Horizontal tab
+    CTL_VT: [0x0b], // Vertical tab
   },
   LINE_SPACING: {
-    LS_DEFAULT: '\x1b\x32',
-    LS_SET: '\x1b\x33'
+    LS_DEFAULT: [0x1b,0x32],
+    LS_SET: [0x1b,0x33]
   },
   HARDWARE: {
-    HW_INIT: '\x1b\x40', // Clear data in buffer and reset modes
-    HW_SELECT: '\x1b\x3d\x01', // Printer select
-    HW_RESET: '\x1b\x3f\x0a\x00', // Reset printer hardware
+    HW_INIT: [0x1b,0x40], // Clear data in buffer and reset modes
+    HW_SELECT: [0x1b,0x3d,0x01], // Printer select
+    HW_RESET: [0x1b,0x3f,0x0a,0x00], // Reset printer hardware
   },
   CASH_DRAWER: {
-    CD_KICK_2: '\x1b\x70\x00', // Sends a pulse to pin 2 []
-    CD_KICK_5: '\x1b\x70\x01', // Sends a pulse to pin 5 []
+    CD_KICK_2: [0x1b,0x70,0x00], // Sends a pulse to pin 2 []
+    CD_KICK_5: [0x1b,0x70,0x01], // Sends a pulse to pin 5 []
   },
   MARGINS: {
-    BOTTOM: '\x1b\x4f', // Fix bottom size
-    LEFT: '\x1b\x6c', // Fix left size
-    RIGHT: '\x1b\x51', // Fix right size
+    BOTTOM: [0x1b,0x4f], // Fi0x bottom size
+    LEFT: [0x1b,0x6c], // Fi0x left size
+    RIGHT: [0x1b,0x51], // Fi0x right size
   },
   PAPER: {
-    PAPER_FULL_CUT: '\x1d\x56\x00', // Full cut paper
-    PAPER_PART_CUT: '\x1d\x56\x01', // Partial cut paper
-    PAPER_CUT_A: '\x1d\x56\x41', // Partial cut paper
-    PAPER_CUT_B: '\x1d\x56\x42', // Partial cut paper
+    PAPER_FULL_CUT: [0x1d,0x56,0x00], // Full cut paper
+    PAPER_PART_CUT: [0x1d,0x56,0x01], // Partial cut paper
+    PAPER_CUT_A: [0x1d,0x56,0x41], // Partial cut paper
+    PAPER_CUT_B: [0x1d,0x56,0x42], // Partial cut paper
   },
   TEXT_FORMAT: {
-    TXT_NORMAL: '\x1b\x21\x00', // Normal text
-    TXT_2HEIGHT: '\x1b\x21\x10', // Double height text
-    TXT_2WIDTH: '\x1b\x21\x20', // Double width text
-    TXT_4SQUARE: '\x1b\x21\x30', // Double width & height text
+    TXT_NORMAL: [0x1b,0x21,0x00], // Normal text
+    TXT_2HEIGHT: [0x1b,0x21,0x10], // Double height text
+    TXT_2WIDTH: [0x1b,0x21,0x20], // Double width text
+    TXT_4SQUARE: [0x1b,0x21,0x30], // Double width & height text
     TXT_CUSTOM_SIZE: function (width, height) { // other sizes
       var widthDec = (width - 1) * 16;
       var heightDec = height - 1;
       var sizeDec = widthDec + heightDec;
-      return '\x1d\x21' + String.fromCharCode(sizeDec);
+      return [0x1d,0x21,String.fromCharCode(sizeDec)]
     },
 
     TXT_HEIGHT: {
-      1: '\x00',
-      2: '\x01',
-      3: '\x02',
-      4: '\x03',
-      5: '\x04',
-      6: '\x05',
-      7: '\x06',
-      8: '\x07'
+      1: [0x00],
+      2: [0x01],
+      3: [0x02],
+      4: [0x03],
+      5: [0x04],
+      6: [0x05],
+      7: [0x06],
+      8: [0x07]
     },
     TXT_WIDTH: {
-      1: '\x00',
-      2: '\x10',
-      3: '\x20',
-      4: '\x30',
-      5: '\x40',
-      6: '\x50',
-      7: '\x60',
-      8: '\x70'
+      1: [0x00],
+      2: [0x10],
+      3: [0x20],
+      4: [0x30],
+      5: [0x40],
+      6: [0x50],
+      7: [0x60],
+      8: [0x70]
     },
 
-    TXT_UNDERL_OFF: '\x1b\x2d\x00', // Underline font OFF
-    TXT_UNDERL_ON: '\x1b\x2d\x01', // Underline font 1-dot ON
-    TXT_UNDERL2_ON: '\x1b\x2d\x02', // Underline font 2-dot ON
-    TXT_BOLD_OFF: '\x1b\x45\x00', // Bold font OFF
-    TXT_BOLD_ON: '\x1b\x45\x01', // Bold font ON
-    TXT_ITALIC_OFF: '\x1b\x35', // Italic font ON
-    TXT_ITALIC_ON: '\x1b\x34', // Italic font ON
-    TXT_FONT_A: '\x1b\x4d\x00', // Font type A
-    TXT_FONT_B: '\x1b\x4d\x01', // Font type B
-    TXT_FONT_C: '\x1b\x4d\x02', // Font type C
-    TXT_ALIGN_LT: '\x1b\x61\x00', // Left justification
-    TXT_ALIGN_CT: '\x1b\x61\x01', // Centering
-    TXT_ALIGN_RT: '\x1b\x61\x02', // Right justification
+    TXT_UNDERL_OFF: [0x1b,0x2d,0x00], // Underline font OFF
+    TXT_UNDERL_ON: [0x1b,0x2d,0x01], // Underline font 1-dot ON
+    TXT_UNDERL2_ON: [0x1b,0x2d,0x02], // Underline font 2-dot ON
+    TXT_BOLD_OFF: [0x1b,0x45,0x00], // Bold font OFF
+    TXT_BOLD_ON: [0x1b,0x45,0x01], // Bold font ON
+    TXT_ITALIC_OFF: [0x1b,0x35], // Italic font ON
+    TXT_ITALIC_ON: [0x1b,0x34], // Italic font ON
+    TXT_FONT_A: [0x1b,0x4d,0x00], // Font type A
+    TXT_FONT_B: [0x1b,0x4d,0x01], // Font type B
+    TXT_FONT_C: [0x1b,0x4d,0x02], // Font type C
+    TXT_ALIGN_LT: [0x1b,0x61,0x00], // Left justification
+    TXT_ALIGN_CT: [0x1b,0x61,0x01], // Centering
+    TXT_ALIGN_RT: [0x1b,0x61,0x02], // Right justification
   }
 }
